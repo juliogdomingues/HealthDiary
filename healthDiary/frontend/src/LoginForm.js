5816741f04a15ed780e7bcb275a19f56efa1b949
julio <32973371+juliogdomingues@users.noreply.github.com>
2024-10-30 21:40:50 +0000

    frontend login

--- healthDiary/frontend/src/LoginForm.js
+++ healthDiary/frontend/src/LoginForm.js
@@ -1,127 +1,142 @@
-import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { TextField, Button } from '@mui/material';
+import './LoginForm.css';
 
 const Login = () => {
-    const [user, setUser] = useState("")
-    const [password, setPassword] = useState("")
-    const [userError, setUserError] = useState(false)
-    const [passwordError, setPasswordError] = useState(false)
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [userError, setUserError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-    // Busca pelo token no LocalStorage antes de toda request, será colocado no header se houver
-    axios.interceptors.request.use(
-        config => {
-            const token = localStorage.getItem("accessToken");
-            if (token) {
-                config.headers['Authorization'] = 'Token ' + token;
-            }
-            return config;
-        },
-        error => {
-            Promise.reject(error)
+  // Configuração do interceptor do axios
+  axios.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        config.headers['Authorization'] = 'Token ' + token;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+
+    setUserError(false);
+    setPasswordError(false);
+
+    if (user === '') {
+      setUserError(true);
+    }
+    if (password === '') {
+      setPasswordError(true);
+    }
+
+    if (user && password) {
+      axios
+        .post('http://localhost:8000/dev/login/', {
+          username: user,
+          password: password,
+        })
+        .then((res) => {
+          localStorage.setItem('accessToken', res.data.token);
+          alert('Login realizado com sucesso');
+        })
+        .catch((error) => {
+          console.log(error);
         });
-
-
-    const loginSubmit = (event) => {
-        event.preventDefault()
-        console.log("Submitted")
-
-        setUserError(false)
-        setPasswordError(false)
-
-        if (user === '') {
-            setUserError(true)
-        }
-        if (password === '') {
-            setPasswordError(true)
-        }
-
-        if (user && password) {
-            console.log(user, password)
-            axios.postForm("http://localhost:8000/dev/login/", {
-                username: user,
-                password: password
-            })
-            .then(res => {
-                localStorage.setItem("accessToken", res.data.token);
-                alert("Login realizado com sucesso")
-            })
-            .catch( error => {
-                console.log(error)
-            })
-        }
     }
+  };
 
-    const AuthRequest = (event) => {
-        event.preventDefault()
-        console.log("Teste de auth")
+  const AuthRequest = (event) => {
+    event.preventDefault();
+    console.log('Teste de auth');
 
-        axios.get("http://localhost:8000/dev/auth_test/")
-        .then(res => {
-            console.log(res.data.success)
-            alert(res.data.success)
-        })
-        .catch( error => {
-            console.log(error)
-            alert(error)
-        })
-    }
+    axios
+      .get('http://localhost:8000/dev/auth_test/')
+      .then((res) => {
+        console.log(res.data.success);
+        alert(res.data.success);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  };
 
-    const LogoutRequest = (event) => {
-        event.preventDefault()
-        
-        //TODO: IMPLEMENTAR REQUEST DE LOGOUT PARA DESCONECTAR USER NO BACKEND
-        
-        localStorage.clear()
-    }
+  const LogoutRequest = (event) => {
+    event.preventDefault();
 
-    return (
-        <React.Fragment>
-            <Card className="LoginCard" elevation={6}>
-                <div className="child">
-                <form autoComplete="off" onSubmit={loginSubmit}>
-                    <h2>Login</h2>
-                    <TextField
-                        label="Username"
-                        onChange={e => setUser(e.target.value)}
-                        required
-                        variant="outlined"
-                        color="secondary"
-                        type="user"
-                        sx={{ mb: 3 }}
-                        fullWidth
-                        value={user}
-                        error={userError}
-                    />
-                    <TextField
-                        label="Password"
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                        variant="outlined"
-                        color="secondary"
-                        type="password"
-                        value={password}
-                        error={passwordError}
-                        fullWidth
-                        sx={{ mb: 3 }}
-                    />
-                    <Button variant="outlined" color="secondary" type="submit">Login</Button>
-                </form>
-                </div>
+    // TODO: IMPLEMENTAR REQUEST DE LOGOUT PARA DESCONECTAR USER NO BACKEND
 
-                <div className="child">  
-                <h2> Opções Adicionais: </h2>
-                <Button variant="outlined" color="primary" onClick={AuthRequest}>Testar autenticação</Button>
-                </div>  
+    localStorage.clear();
+    alert('Logout realizado com sucesso');
+  };
 
-                <div className="child">  
-                <Button variant="outlined" color="primary" onClick={LogoutRequest}>Logout</Button>
-                </div>  
-            </Card>
-        </React.Fragment>
-    );
-}
+  return (
+    <div className="login-container">
+      <div className="left-side">
+        <div className="text">
+          <h1>Health Diary</h1>
+          <p>Monitore seus sintomas.</p>
+        </div>
+      </div>
+      <div className="right-side">
+        <img src="logo192.png" alt="Logo" className="logo" />
+        <form autoComplete="off" onSubmit={loginSubmit} className="login-form">
+          <TextField
+            label="Username"
+            onChange={(e) => setUser(e.target.value)}
+            required
+            variant="outlined"
+            color="secondary"
+            type="text"
+            fullWidth
+            value={user}
+            error={userError}
+            sx={{ mb: 3 }}
+          />
+          <TextField
+            label="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            variant="outlined"
+            color="secondary"
+            type="password"
+            fullWidth
+            value={password}
+            error={passwordError}
+            sx={{ mb: 3 }}
+          />
+          <Button variant="contained" color="primary" type="submit" fullWidth>
+            Login
+          </Button>
+        </form>
+        <div className="additional-options">
+          <Button variant="outlined" color="secondary" onClick={AuthRequest} fullWidth>
+            Testar autenticação
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={LogoutRequest}
+            fullWidth
+            style={{ marginTop: '10px' }}
+          >
+            Logout
+          </Button>
+        </div>
+        <div className="footer">
+          <small> © Todos os direitos reservados.</small>
+          <a href="mailto:jgdjulio@gmail.com">Entre em contato</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
